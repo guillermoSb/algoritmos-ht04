2bb7cd76e5d43eff5e48d7e77422e1a5e3c88a20
lourdessaa11 <saa21333@uvg.edu.gt>
2022-02-17 08:07:13 -0600

    Código reutilizable de la HDT2

--- src/com/ht/InfixCalculator.java
+++ src/com/ht/InfixCalculator.java
@@ -1,4 +1,99 @@
 package com.ht;
+import java.util.ArrayList;
 
-public class InfixCalculator {
+public class InfixCalculator implements Calculator {
+
+    ArrayList<Double> data;
+
+    InfixCalculator() {
+        data = new ArrayList<Double>();
+    }
+
+    public void add(Double item) {
+        data.add(item);
+    }
+
+    public Double remove() {
+        return data.remove(size() - 1);
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public double calculate(String operation) {
+        // Split the operation into subcomponents
+        String[] splittedOperation = operation.split(" ");
+        // Iterate through each value on the operation
+        for (String value:
+             splittedOperation) {
+            // TODO: Handle error
+            if (!isNumber(value) && !isOperator(value)) {
+                return 0;
+            }
+
+            // If the value is a number, add it to the stack
+            if (isNumber(value)) {
+               add(Double.parseDouble(value));
+            }
+            // If the value is an operator, start operating two values
+            if(isOperator(value)) {
+                // Todo: Handle Exception
+                Double operatorA = remove();
+                Double operatorB = remove();
+                String operator = value;
+                Double result = operate(operatorA, operatorB, operator);
+                add(result);
+            }
+        }
+        return remove();
+    }
+
+    /**
+     * Operate two values
+     * @param a Operator 1
+     * @param b Operator 2
+     * @param operator operation. +, -, /, *
+     * @return
+     */
+    private double operate(Double a, Double b, String operator) {
+        if (operator.equals("+")) return a + b;
+        if (operator.equals("-")) return a + b;
+        if (operator.equals("*")) return a * b;
+        if (operator.equals("/")) return a / b;
+        return 0;   // Last instance
+    }
+
+
+    /**
+     * Check if a string is a number or not
+     * @param value
+     * @return
+     */
+    private boolean isNumber(String value) {
+        try {
+            Double number = Double.parseDouble(value);
+            return true;
+        } catch (Exception e) {
+            return  false;
+        }
+    }
+
+    /**
+     * Check if a string is an operator or not
+     * @param value
+     * @return
+     */
+    private boolean isOperator(String value) {
+        if (value.equals("+") || value.equals("-") || value.equals("*") || value.equals("/")) {
+            return true;
+        }
+        return false;
+    }
+
+    public void transformExpression(String operation){
+
+
+    }
 }
