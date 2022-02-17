18017a09ed4eeee73dad4ce65c60419000aeffed
lourdessaa11 <saa21333@uvg.edu.gt>
2022-02-17 08:08:01 -0600

    Código reutilizable de la HDT2

--- src/com/ht/Calculator.java
+++ src/com/ht/Calculator.java
@@ -1,4 +1,11 @@
 package com.ht;
 
 public interface Calculator {
+
+    /**
+     * Calculates an entire operation.
+     * @param operation String containing the operation to be calculated.
+     * @return double with the result of the operation
+     */
+    public double calculate(String operation);
 }
